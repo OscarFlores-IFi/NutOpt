3419fb294cdedc6f1336dd542ec0a97607c501ae
OscarFlores-IFi <if715029@gmail.com>
2023-12-02 17:23:27 +0100

    ready to add optimizer

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,17 +31,19 @@ def replace_underscores_with_spaces(text):
     return text.replace("_", " ")
 
 # Function to calculate the current nutrient amount based on user-defined food quantities
-def calculate_current_amount(nutrient, identifier):
-    min_limit, max_limit = st.session_state.nutrient_limits[identifier]    
+def calculate_current_amount(nutrient):
     current_value = sum(foods[food][nutrient] * st.session_state.food_quantities.get(food, 0) for food in foods)
-    percentage = (current_value / max_limit) 
-    return min(percentage, 1)
+    percentage = (current_value / round(nutrient_constraints[nutrient]['max']*1.3))
+    return (current_value, min(percentage, 1))
 
-def get_bar_color(current_amount):
-    if current_amount < 1:
-        return 'green'
+def get_text_to_show(identifier, current_amount):
+    min_limit, max_limit = st.session_state.nutrient_limits[identifier]
+    if current_amount <= min_limit:
+        return ":orange[below limits]"  
+    elif current_amount >= max_limit:
+        return ":orange[above limits]"      
     else:
-        return 'yellow'  # Change to a darker shade for amounts exceeding 100%
+        return ":green[within limits]"
 
 
 
@@ -58,9 +60,9 @@ def main():
 
     # User input for food quantities
     if selected_food in st.session_state.food_quantities:
-        st.session_state.food_quantities[selected_food] = st.slider(f"Quantity of {selected_food}", 0, 200, st.session_state.food_quantities[selected_food])
+        st.session_state.food_quantities[selected_food] = st.slider(f"Quantity of {selected_food}", 0, 400, st.session_state.food_quantities[selected_food])
     else:
-        st.session_state.food_quantities[selected_food] = st.slider(f"Quantity of {selected_food}", 0, 200, 0)
+        st.session_state.food_quantities[selected_food] = st.slider(f"Quantity of {selected_food}", 0, 400, 0)
 
     # Display existing sliders
     st.subheader("Current Food Quantities:")
@@ -76,23 +78,21 @@ def main():
 
 
     # Sliders for nutrient limits
-    st.subheader("Set Nutrient Limits:")
+    st.subheader("Nutrient Limits:")
     for nutrient, limits in nutrient_constraints.items():
         identifier = replace_spaces_and_commas_with_underscores(nutrient)
-        st.session_state.nutrient_limits[identifier] = st.slider(f"Set {nutrient} limit", 0, int(limits['max']*1.3), (int(limits['min']), int(limits['max'])))
+        st.session_state.nutrient_limits[identifier] = st.slider(f"{nutrient} limits",                          \
+                                                                 0,                                             \
+                                                                 int(limits['max']*1.3),                        \
+                                                                 (int(limits['min']), int(limits['max'])))
    
-        current_amount = calculate_current_amount(nutrient, identifier)
-
-        st.markdown("""
-        <style>
-        .stProgress .st-bo {
-            background-color: get_bar_color(current_amount);
-        }
-        </style>
-        """, unsafe_allow_html=True)
+        current_amount, percentage_of_max_lim = calculate_current_amount(nutrient)
+        
+        text_to_show = get_text_to_show(identifier, current_amount)
         
         # Use st.progress for the progress bar
-        st.progress(int(current_amount*100))           
+        st.progress(int(percentage_of_max_lim*100), text=text_to_show)    
+        # st.progress.text(":green[{str(round(current_amount))}]")
 
     # # Button to optimize
     # if st.button("Optimize"):
