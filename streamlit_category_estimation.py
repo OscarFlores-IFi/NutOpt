5e152b824fc24c719061acf4f122979b3bbec75b
OscarFlores-IFi <if715029@gmail.com>
2023-12-31 14:39:21 +0100

    max is working, but min has to be reframed.

---
+++ streamlit_category_estimation.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 22:19:44 2023
+
+@author: Oscar Flores
+
+This is the draft that will later become a front end for the optimizer
+"""
+import streamlit as st
+import json
+from scipy.optimize import linprog
+import numpy as np
+
+directory = r'C:\Users\52331\\'
+
+
+
+#%% Define functions
+
+# Function to replace spaces and commas with underscores
+def replace_spaces_and_commas_with_underscores(text):
+    return text.replace(" ", "_").replace(",", "_")
+
+# Function to replace underscores with spaces
+def replace_underscores_with_spaces(text):
+    return text.replace("_", " ")
+
+# # Function to calculate the current category amount based on user-defined food quantities
+# def calculate_current_amount_categories(category, food_categories):
+#     current_value = sum(food_categories[food][category] * st.session_state.food_quantities.get(food, 0) for food in food_categories)
+#     return (current_value)
+
+# Function to calculate the current category amount based on user-defined food quantities
+def calculate_current_macros(macronutrient):
+    multiplier = {'Proteinas': 4, 'Lipidos': 9, 'Carbohidratos': 4}
+
+    current_macro = sum(st.session_state.smae_categories.get(category, 0) *  \
+                    smae_categories[replace_underscores_with_spaces(category)].get(macronutrient)  \
+                    for category in st.session_state.smae_categories.keys())
+        
+    current_energy_per_macro = sum(multiplier[macronutrient] *  \
+                    st.session_state.smae_categories.get(category, 0) *  \
+                    smae_categories[replace_underscores_with_spaces(category)].get(macronutrient)  \
+                    for category in st.session_state.smae_categories.keys())
+
+    
+    return round(current_macro), round(current_energy_per_macro) 
+
+
+def get_text_to_show(identifier, current_amount):
+    min_limit, max_limit = st.session_state.category_limits[identifier]
+    if current_amount < min_limit:
+        return ":orange[below limits]"  
+    elif current_amount > max_limit:
+        return ":orange[above limits]"      
+    else:
+        return ":green[within limits]"
+
+def calculate_min_max(category, identifier, limits):
+    multiplier = {'Proteinas': 4, 'Lipidos': 9, 'Carbohidratos': 4}
+
+    lhs_ineq = []
+    rhs_ineq = []
+       
+    for macronutrient in limits:
+        list_of_categories = []
+        
+        for cat in smae_categories:
+            list_of_categories.append(smae_categories[cat][macronutrient] * multiplier[macronutrient]) 
+        
+        lhs_ineq.append(list_of_categories)
+        lhs_ineq.append([-i for i in list_of_categories])
+        
+        _, current_energy_per_macro = calculate_current_macros(macronutrient)
+        
+        min_ = limits[macronutrient]['min']
+        max_ = limits[macronutrient]['max']
+        
+        rhs_ineq.append(max_ - current_energy_per_macro)
+        rhs_ineq.append(current_energy_per_macro - min_)
+                
+    # Boundaries.
+    bnd = [(0,100)]*len(lhs_ineq[0]) # set boundaries for each of the foods between (a,b)
+
+    #################    
+    # FOR THE MAXIMUM:
+    # Minimization of objective Function
+    obj = np.zeros(len(smae_categories))
+    idx = list(smae_categories.keys()).index(category)
+    obj[idx] = -1
+
+    opt = linprog(c=obj, 
+                  A_ub=lhs_ineq, 
+                  b_ub=rhs_ineq,
+                  bounds=bnd,
+                  method = 'highs')
+    
+    # return ({i:np.round(j,2) for i,j in zip(list_of_foods,opt.x) if j > 0})
+
+    # opt = linprog(c=obj, 
+    #               A_ub=lhs_ineq, 
+    #               b_ub=rhs_ineq,
+    #               bounds=bnd,
+    #               method = 'highs',
+    #               integrality=1)      
+    try: 
+        maximum = opt.x[idx] + st.session_state.smae_categories.get(identifier, 0)
+    except:
+        print("Limite ha sido superado, reduzca cantidad.")
+        maximum = st.session_state.smae_categories.get(identifier, 0)
+    
+    #################
+    # FOR THE MINIMUM
+    obj[idx] = 1
+
+    opt = linprog(c=obj, 
+                  A_ub=lhs_ineq, 
+                  b_ub=rhs_ineq,
+                  bounds=bnd,
+                  method = 'highs')
+    
+    # return ({i:np.round(j,2) for i,j in zip(list_of_foods,opt.x) if j > 0})
+
+    # opt = linprog(c=obj, 
+    #               A_ub=lhs_ineq, 
+    #               b_ub=rhs_ineq,
+    #               bounds=bnd,
+    #               method = 'highs',
+    #               integrality=1)  
+    
+    try: 
+        minimum = opt.x[idx]
+    except:
+        print("No se qpd.")
+        minimum = 0
+    
+    #################
+    return(np.floor(minimum), np.floor(maximum))
+
+    # return ({i:round(j) for i,j in zip(list_of_foods,opt.x) if j >0})
+    
+@st.cache_resource
+def read_json(filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+#%% Read files
+# Food categories
+smae_categories = read_json(directory + "smae_equivalents.json")
+
+#%% Streamlit app
+def main():
+                     
+    st.title("Nutritional Optimization App")
+    
+    # Calories = st.text_input('Calorie consumption:', 2000)
+    Calories = 2000
+
+    limits = {"Proteinas":{"min":Calories*0.1,"max":Calories*0.35}, 
+              "Lipidos":{"min":Calories*0.2,"max":Calories*0.35}, 
+              "Carbohidratos":{"min":Calories*0.45,"max":Calories*0.65}}
+    
+    if st.button("Reset"):
+        del st.session_state.smae_categories
+        # Delete all the items in Session state
+        for key in st.session_state.keys():
+            del st.session_state[key]
+
+    # Dropdown for selecting a food
+    # selected_food = st.selectbox("Selecciona una comida:", list(food_categories.keys()))
+    
+    # # Create or get the food_quantities dictionary from session state
+    if 'smae_categories' not in st.session_state:
+        st.session_state.smae_categories = {}
+
+    # User input for food quantities
+    
+    print("\n")
+
+    for category in smae_categories.keys():
+        identifier = replace_spaces_and_commas_with_underscores(category)
+        
+        minimum, maximum = calculate_min_max(category, identifier, limits)
+        st.write(f"{category}, Min: {minimum}, Max:{maximum}")
+        
+        st.session_state.smae_categories[identifier] = st.slider(f"Cantidad de {category}", 0, 10)
+        
+
+        
+    
+    for macro in limits: 
+        
+        current_macros, _ = calculate_current_macros(macro)
+        st.write(f"{macro}: {current_macros}")  
+        # text_to_show = get_text_to_show(identifier, current_amount)
+          
+        # Use st.progress for the progress bar
+        # st.progress(percentage_of_max_lim, text=text_to_show)    
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
+#%%
+# python -m streamlit run C:\Users\52331\Documents\GitHub\NutOpt\streamlit_category_estimation.py
